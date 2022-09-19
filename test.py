fe80e0ef2b3c3885c14348216d8b617800f07988
benjamincordoba <113872422+benjamincordoba@users.noreply.github.com>
2022-09-18 23:10:49 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 from conversor import conversor as methodTest
 import pytest
 
-def test_conversor():
-    assert methodTest("colombianos", 3875, 1000) == 0.26
+def test_conversor(self):
+    module.input = lambda: '1'
+    output = methodTest("colombianos", 3875, 1000)
+    assert output == 0.26
